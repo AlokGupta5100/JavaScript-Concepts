bb9edd4db82436b6afbfc5b36a0f0b79d308897c
Alok <alokgupta5100@gmail.com>
2024-01-20 23:09:18 +0530

    chore: iife function

---
+++ Functions/IIFE.js
@@ -0,0 +1,18 @@
+//immediately invoke function expression
+
+(function voke(){
+    console.log("this is Imm.. invoke function")
+})();
+//named IIFE
+
+( () => {
+    console.log(`work in bracket scopes`)
+})();
+//unnamed IIFE
+
+( (country) => {
+    console.log(`work in bracket scopes ${country}`)
+})('INDIA');
+//parameter IIFE
+//commas required for strict the scope
+//help from pollutions of global variables or declarations
